fd29723f87594eba9b293a8fb48d7123e9f94e8b
roLLlyTHuK <ircvipua@gmail.com>
2023-08-26 12:37:27 +0300

    +task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+const saveFormState = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+const savedState = localStorage.getItem('feedback-form-state');
+
+if (savedState) {
+  const parsedState = JSON.parse(savedState);
+  emailInput.value = parsedState.email;
+  messageInput.value = parsedState.message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formState);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
