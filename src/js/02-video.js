0633ab2ea72fd2d01de21c5e349fdf2be32f1f8e
roLLlyTHuK <ircvipua@gmail.com>
2023-08-26 11:56:50 +0300

    +task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const player = new Vimeo('vimeo-player');
+// ! Зберігання позиції
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then(time => {
+        localStorage.setItem('videoplayer-current-time', time);
+    });
+}, 1000));
+
+//! викликаємо позицію
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
 
